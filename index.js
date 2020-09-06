cfb4408dbd0b6ddf47f2339f9b8972e6cc27446e
chungil <chungil13@naver.com>
2020-09-07 03:06:26 +0900

    예제 진행 중

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hi!");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  //   console.log(req);
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("Your are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(4000, handleListening);
+
+// // respond with "hello world" when a GET request is made to the homepage
+// app.get("/", function (req, res) {
+//   res.send("hello world");
+// });
