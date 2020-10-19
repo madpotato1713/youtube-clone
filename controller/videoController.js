2334e68038bd1e3bf4c16f290b1977f93432671e
chungil <chungil13@naver.com>
2020-10-20 00:14:48 +0900

    20201020 예제 진행 중

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,13 +1,16 @@
+import { videos } from "../db";
+
 // export const home = (req, res) => res.send("HOME");
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+
 export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
   res.render("search", { pageTitle: "Search", searchingBy });
 };
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
 
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Potato",
+      email: "potato@vegi.com",
+    },
+  },
+  {
+    id: 1245143,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Potato",
+      email: "potato@vegi.com",
+    },
+  },
+  {
+    id: 2413312,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Potato",
+      email: "potato@vegi.com",
+    },
+  },
+  {
+    id: 4354532,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Potato",
+      email: "potato@vegi.com",
+    },
+  },
+];
